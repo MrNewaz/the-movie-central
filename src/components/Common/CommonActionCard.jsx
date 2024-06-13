b489a55d698a8ef37267d325cd893095f86befc4
Saif Rahman <srahman@powerley.com>
2024-06-13 18:00:15 +0600

    Watchlist page added

---
+++ src/components/Common/CommonActionCard.jsx
@@ -0,0 +1,108 @@
+import { Button, Grid } from "@mui/material"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { imagePosterPath } from "../../services/api"
+import { useFirestore } from "../../services/firestore"
+
+const CommonActionCard = ({ item, user }) => {
+  const [isInWatchlist, setIsInWatchlist] = useState(false)
+  const { checkIfInWatchlist, removeFromWatchlist } = useFirestore()
+
+  const handleRemoveFromWatchlist = async () => {
+    await removeFromWatchlist(user?.uid, item.id)
+    const isSetToWatchlist = await checkIfInWatchlist(user?.uid, item.id)
+    setIsInWatchlist(isSetToWatchlist)
+  }
+
+  useEffect(() => {
+    if (!user) {
+      setIsInWatchlist(false)
+      return
+    }
+
+    checkIfInWatchlist(user?.uid, item.id).then((data) => {
+      setIsInWatchlist(data)
+    })
+  }, [item, user, checkIfInWatchlist])
+
+  return (
+    <Grid
+      component={Link}
+      to={`/${item.type}/${item?.id}`}
+      item
+      xs={12}
+      sm={3}
+      key={item.id}
+    >
+      <Box
+        sx={{
+          "& img": {
+            width: "100%",
+            height: "183px",
+            objectFit: "cover",
+            borderRadius: "16px",
+          },
+        }}
+      >
+        {item?.poster_path ? (
+          <img src={imagePosterPath + item?.poster_path} alt={item?.title} />
+        ) : (
+          <Box
+            sx={{
+              height: "183px",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              bgcolor: "#F4F4F4",
+              borderRadius: "16px",
+              mb: 1,
+            }}
+          >
+            <Typography
+              variant="h6"
+              align="center"
+              sx={{
+                color: "#9CA4AB",
+              }}
+            >
+              No Image
+            </Typography>
+          </Box>
+        )}
+
+        <Typography gutterBottom>{item?.title || item?.name}</Typography>
+        <Typography
+          align="left"
+          sx={{
+            fontSize: "12px",
+            fontWeight: "medium",
+            color: "#9CA4AB",
+          }}
+        >
+          ⭐ {item?.vote_average?.toFixed(1)} |{" "}
+          {new Date(item?.release_date).getFullYear()} •{" "}
+          {item?.media_type === "movie" ? "Movie" : "Tv Series"}
+        </Typography>
+
+        {isInWatchlist && (
+          <Button
+            variant="outlined"
+            color="error"
+            fullWidth
+            sx={{
+              mt: 2,
+            }}
+            onClick={handleRemoveFromWatchlist}
+          >
+            Remove from WatchList
+          </Button>
+        )}
+      </Box>
+    </Grid>
+  )
+}
+
+export default CommonActionCard
--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,13 +1,62 @@
-import Box from "@mui/material/Box"
-import Typography from "@mui/material/Typography"
+import { CircularProgress, Container, Grid, Typography } from "@mui/material"
+
+import { useEffect, useState } from "react"
+
+import CommonActionCard from "../components/Common/CommonActionCard"
+import { useAuth } from "../context/useAuth"
+import { useFirestore } from "../services/firestore"
 
 const WatchList = () => {
+  const { getWatchlist } = useFirestore()
+  const { user } = useAuth()
+  const [watchlist, setWatchlist] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (user?.uid) {
+      getWatchlist(user?.uid)
+        .then((data) => {
+          setWatchlist(data)
+          console.log(data, "data")
+        })
+        .catch((err) => {
+          console.log(err, "error")
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    }
+  }, [user?.uid, getWatchlist, watchlist])
+
+  if (isLoading) {
+    return (
+      <Container
+        maxWidth="xl"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50vh",
+        }}
+      >
+        <CircularProgress />
+      </Container>
+    )
+  }
   return (
-    <Box>
-      <Typography align="left" variant="h2">
-        WatchList
+    <Container maxWidth="xl" sx={{ py: 10 }}>
+      <Typography align="left" variant="h4" gutterBottom>
+        Your Watchlist
       </Typography>
-    </Box>
+
+      {!isLoading && (
+        <Grid container spacing={3}>
+          {watchlist.map((item) => (
+            <CommonActionCard key={item.id} item={item} user={user} />
+          ))}
+        </Grid>
+      )}
+    </Container>
   )
 }
 
