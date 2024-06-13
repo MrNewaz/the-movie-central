fb20257c848aff33318d6886b793d2c6c0b4f7f2
Saif Rahman <srahman@powerley.com>
2024-06-13 17:33:07 +0600

    Watchlist added to protected routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,13 +5,21 @@ import Home from "./pages/Home"
 import MovieReleases from "./pages/MovieReleases"
 import Search from "./pages/Search"
 import WatchList from "./pages/WatchList"
+import Protected from "./routes/Protected"
 
 const AppRoutes = () => {
   return (
     <main>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/watchlist" element={<WatchList />} />
+        <Route
+          path="/watchlist"
+          element={
+            <Protected>
+              <WatchList />
+            </Protected>
+          }
+        />
         <Route path="/discover" element={<Discover />} />
         <Route path="/movie-release" element={<MovieReleases />} />
         <Route path="/:type/:id" element={<Details />} />
---
+++ src/routes/Protected.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types"
+import { Navigate } from "react-router-dom"
+import { useAuth } from "../context/useAuth"
+
+const Protected = ({ children }) => {
+  const { user, isLoading } = useAuth()
+
+  if (isLoading) {
+    return null
+  }
+
+  return <>{user ? children : <Navigate to={"/"} />}</>
+}
+
+export default Protected
+
+Protected.propTypes = {
+  children: PropTypes.node.isRequired,
+}
