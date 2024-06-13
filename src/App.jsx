8c38a579bb7e07ad8fa5b39f3127db09c530246a
Saif Rahman <srahman@powerley.com>
2024-06-13 16:59:04 +0600

    Added theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import AppRoutes from "./Routes"
+import { CssBaseline } from "@mui/material"
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
+import AppRoutes from "./Routes"
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#00925D",
+      },
+      secondary: {
+        main: "#ffffff",
+      },
+      background: {
+        default: "#000000",
+      },
+    },
+    typography: {
+      fontFamily: "Rubik",
+      button: {
+        textTransform: "none",
+      },
+    },
+  })
   return (
     <>
-      <Navbar />
-      <AppRoutes />
-      <Footer />
+      <ThemeProvider theme={responsiveFontSizes(theme)}>
+        <CssBaseline />
+        <Navbar />
+        <AppRoutes />
+        <Footer />
+      </ThemeProvider>
     </>
   )
 }
